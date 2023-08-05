e8f23b7e18ad16f19eaebd59ec49f69e8320331f
Alex Ehlke <alex.ehlke@gmail.com>
2023-08-04 21:07:06 -0400

    Update EnvironmentValues+PageSheet.swift

--- Sources/PageSheetCore/EnvironmentValues+PageSheet.swift
+++ Sources/PageSheetCore/EnvironmentValues+PageSheet.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
 private struct SelectedDetentIdentifier: EnvironmentKey {
-  static let defaultValue: PageSheet.Detent.Identifier? = nil
+  static let defaultValue: PageSheet.Detent.Identifier = .large()
 }
 
 extension EnvironmentValues {
-  internal var _selectedDetentIdentifier: PageSheet.Detent.Identifier? {
+  internal var _selectedDetentIdentifier: PageSheet.Detent.Identifier {
     get { self[SelectedDetentIdentifier.self] }
     set { self[SelectedDetentIdentifier.self] = newValue }
   }
@@ -15,7 +15,7 @@ extension EnvironmentValues {
 
 extension EnvironmentValues {
   /// The current ``Detent`` identifier of the sheet presentation associated with this environment.
-  public var selectedDetentIdentifier: PageSheet.Detent.Identifier? {
+  public var selectedDetentIdentifier: PageSheet.Detent.Identifier {
     self[SelectedDetentIdentifier.self]
   }
 }
